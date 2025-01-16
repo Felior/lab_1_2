e31f164379f504b1b7db5e6166f0d7ba1c5ace61
Felior <130604991+Felior@users.noreply.github.com>
2025-01-16 19:48:08 +0300

    Add files via upload

---
+++ src/main/java/example/App.java
@@ -0,0 +1,17 @@
+package example;
+
+public class App {
+    private String string;
+
+    public App(){
+        string = "unnamed";
+    }
+
+    public String getString(){
+        return string;
+    }
+
+    public void setString(String string){
+        this.string = string;
+    }
+}
---
+++ src/test/java/example/AppTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+public class AppTest {
+
+    @Test
+    public void setString(){
+        Assertions.assertEquals(0, 0);
+    }
+
+}
